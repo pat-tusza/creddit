7e95a31313b3244ed87acbb1009d73a4f9cbb4b4
Patrick Tuszakowski <mrtall500@gmail.com>
2021-02-12 14:51:18 -0500

    fixed tables

--- db/migrate/20210212170416_create_posts.rb
+++ db/migrate/20210212170416_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.belongs_to :user_id, null: false, foreign_key: true
-      t.belongs_to :activity_id, null: false, foreign_key: true
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :activity, null: false, foreign_key: true
       t.string :title
       t.string :body
 
--- db/migrate/20210212170506_create_comments.rb
+++ db/migrate/20210212170506_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-      t.belongs_to :user_id, null: false, foreign_key: true
-      t.belongs_to :post_id, null: false, foreign_key: true
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :post, null: false, foreign_key: true
       t.string :body
 
       t.timestamps
